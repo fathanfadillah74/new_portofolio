a360dd01b84d2da7a1de171b528489474c9e2e24
fathanfadillah74 <fathanfadillah74@gmail.com>
2024-11-24 01:09:39 +0700

    update done navbar & about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,42 +1,78 @@
-import { useEffect, useState } from 'react';
-import fotoProfile from '../assets/img/fathan.jpeg'
-import fotoProfile2 from '../assets/img/fathan2.jpeg'
-import '../assets/style/about.css'
+import { useEffect, useState } from "react";
+import fotoProfile from "../assets/img/fathan.jpeg";
+import fotoProfile2 from "../assets/img/fathan2.jpeg";
+import fotoProfile3 from "../assets/img/fathan3.jpeg";
+import fotoProfile4 from "../assets/img/fathan4.jpeg";
+import fotoProfile5 from "../assets/img/fathan5.jpeg";
+import fotoProfile6 from "../assets/img/fathan6.jpeg";
+import "../assets/style/about.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-cards";
+import { EffectCards } from "swiper/modules";
 
 const About = () => {
-    const [text, setText] = useState("");
-    const fullText =
-        "Im Fathan Fadillah, and I'm a software developer. Born in Jakarta on May 23, 2002, I graduated from Telkom University in 2020 with a diploma in information systems. Becoming a developer is a challenge in itself for me, where time is spent studying to increase my knowledge and survive in this field. but because of that, I also feel comfortable being part of this field.";
+  const [text, setText] = useState("");
+  const fullText =
+    "Im Fathan Fadillah, and I'm a software developer. Born in Jakarta on May 23, 2002, I graduated from Telkom University in 2020 with a diploma in information systems. Becoming a developer is a challenge in itself for me, where time is spent studying to increase my knowledge and survive in this field. but because of that, I also feel comfortable being part of this field.";
 
-    useEffect(() => {
-        let index = 0;
-        const typingEffect = setInterval(() => {
-            if (index < fullText.length - 1) {
-                setText((prev) => prev + fullText[index]);
-                index++;
-            } else {
-                clearInterval(typingEffect);
-                document.getElementById("cursor").style.display = "none";
-            }
-        }, 30);
+  useEffect(() => {
+    let index = 0;
+    const typingEffect = setInterval(() => {
+      if (index < fullText.length - 1) {
+        setText((prev) => prev + fullText[index]);
+        index++;
+      } else {
+        clearInterval(typingEffect);
+        document.getElementById("cursor").style.display = "none";
+      }
+    }, 30);
 
-        return () => clearInterval(typingEffect);
-    }, [fullText]);
-    return (
-        <>
-            <div className="big-container">
-                <div className="photo-container">
-                    <img src={fotoProfile} alt="" />
-                    <img src={fotoProfile2} alt="" />
-                </div>
-                <div className="about-container">
-                    <div className="about-text">
-                        <p>{text}<span id='cursor' className="blinking-cursor"></span></p>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
+    return () => clearInterval(typingEffect);
+  }, [fullText]);
+  return (
+    <>
+      <div className="big-container">
+        <div className="photo-container">
+          <Swiper
+            spaceBetween={50}
+            onSlideChange={() => console.log("slide change")}
+            onSwiper={(swiper) => console.log(swiper)}
+            modules={[EffectCards]}
+            effect={"cards"}
+            grabCursor={true}
+          >
+            <SwiperSlide>
+              <img src={fotoProfile} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={fotoProfile2} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={fotoProfile3} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={fotoProfile4} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={fotoProfile5} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={fotoProfile6} alt="" />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+        <div className="about-container">
+          <div className="about-text">
+            <p>
+              {text}
+              <span id="cursor" className="blinking-cursor"></span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default About
+export default About;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,45 +1,77 @@
-import '../assets/style/navbar.css';
-import burgerIcon from '../assets/img/burgerIcon.png';
-import dayIcon from '../assets/img/dayIcon.png';
-import nightIcon from '../assets/img/nightIcon.png';
-import closeIcon from '../assets/img/closeIcon.png';
-import { useState } from 'react';
+import "../assets/style/navbar.css";
+import burgerIcon from "../assets/img/burgerIcon.png";
+import dayIcon from "../assets/img/dayIcon.png";
+import nightIcon from "../assets/img/nightIcon.png";
+import closeIcon from "../assets/img/closeIcon.png";
+import { useState, useEffect } from "react";
 
 function Navbar() {
-    const [isNavVisible, setNavVisible] = useState(false);
-    const [activeIndex, setActiveIndex] = useState(null);
+  const [isNavVisible, setNavVisible] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 768); // Track screen size
 
-    const handleNavItemClick = (index) => {
-        setActiveIndex(index);
+  // Handle window resizing
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth >= 768);
     };
-    const toggleNav = () => {
-        setNavVisible(!isNavVisible);
-    };
-    return (
-        <nav className="navbar">
-            <div className="name">Fathan Fadillah</div>
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const handleNavItemClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  const toggleNav = () => {
+    setNavVisible(!isNavVisible);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="name">Fathan Fadillah</div>
+      {!isDesktop && (
+        <div className="theme-toggle">
+          <input type="checkbox" id="themeSwitch" />
+          <label htmlFor="themeSwitch" className="switch">
+            <span className="slider">
+              <img className="dayIcon" src={dayIcon} alt="Day Icon" />
+              <img className="nightIcon" src={nightIcon} alt="Night Icon" />
+            </span>
+          </label>
+        </div>
+      )}
+      <img
+        className="burger"
+        src={isNavVisible ? closeIcon : burgerIcon}
+        alt=""
+        onClick={toggleNav}
+      />
+      <ul className={`navLinks ${isNavVisible ? "show" : ""}`}>
+        {isDesktop && (
             <div className="theme-toggle">
-                <input type="checkbox" id="themeSwitch" />
-                <label for="themeSwitch" className="switch">
-                    <span className="slider">
-                        <img className="dayIcon" src={dayIcon} alt="Day Icon" />
-                        <img className="nightIcon" src={nightIcon} alt="Night Icon" />
-                    </span>
-                </label>
+              <input type="checkbox" id="themeSwitch" />
+              <label htmlFor="themeSwitch" className="switch">
+                <span className="slider">
+                  <img className="dayIcon" src={dayIcon} alt="Day Icon" />
+                  <img className="nightIcon" src={nightIcon} alt="Night Icon" />
+                </span>
+              </label>
             </div>
-            <img className='burger' src={isNavVisible ? closeIcon : burgerIcon} alt="" onClick={toggleNav} />
-            <ul className={`navLinks ${isNavVisible ? 'show' : ''}`}>
-                {['About', 'Skill', 'Experience', 'Contact'].map((item, index) => (
-                    <li
-                        key={index}
-                        className={activeIndex === index ? 'is-active' : ''}
-                        onClick={() => handleNavItemClick(index)}>
-                        <a href={`#${item.toLowerCase()}`}>{item}</a>
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    );
+        )}
+        {["About", "Skill", "Experience", "Contact"].map((item, index) => (
+          <li
+            key={index}
+            className={activeIndex === index ? "is-active" : ""}
+            onClick={() => handleNavItemClick(index)}
+          >
+            <a href={`#${item.toLowerCase()}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
